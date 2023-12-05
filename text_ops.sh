eeb10d149e98e9292ae1b8bbc15dabcab5c35234
Solomon <rebekrebeloff@gmail.com>
2023-12-05 11:44:09 +0300

    Correcting 5F

--- text_ops.sh
+++ text_ops.sh
@@ -3,5 +3,5 @@ grep -coiw function README.md > output/function-keyword-count.txt
 grep '^#[^#]' README.md | sed 's/^#//' > output/unique-headers.txt
 grep -Eo '[0-9]+' README.md | sort -n > output/sorted-numbers.txt
 grep -A 10000 FEATURES README.md | tr '\n' ' ' >  output/features-summary.txt
-grep -E "Calculator" README.md | sort -r > output/calculator-lines.txt3
-find output -type f -exec cat {} \;
+grep -E "Calculator" README.md | sort -r > output/calculator-lines.txt
+find output -type f -exec cat {} \; > output/print_content_of_folder.txt
