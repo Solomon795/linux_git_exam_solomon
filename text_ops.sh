bec19f6433404b1e821ad6439489cf380933a418
Solomon <rebekrebeloff@gmail.com>
2023-12-05 11:38:47 +0300

    Added 5B

--- text_ops.sh
+++ text_ops.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 grep -coiw function README.md > output/function-keyword-count.txt
+grep '^#[^#]' README.md | sed 's/^#//' > output/unique-headers.txt
 grep -Eo '[0-9]+' README.md | sort -n > output/sorted-numbers.txt
 grep -A 10000 FEATURES README.md | tr '\n' ' ' >  output/features-summary.txt
 grep -E "Calculator" README.md | sort -r > output/calculator-lines.txt3
